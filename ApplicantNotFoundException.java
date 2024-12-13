accea2b8470fd9b8435803e82a9b4298ae84ad63
Manas Singh <83864851+KrossKinetic@users.noreply.github.com>
2024-12-13 17:04:33 -0500

    Update ApplicantNotFoundException.java
    Removed personal information

--- ApplicantNotFoundException.java
+++ ApplicantNotFoundException.java
@@ -1,11 +1,6 @@
 /**
  * The {@code ApplicantNotFoundException} class represents an exception that is thrown when
  * an applicant is not found in the hiring table.
- * 
- * @author Manas Singh
- *    e-mail: anshumaan.singh@stonybrook.edu
- *    Stony Brook ID: 115426564
- *    Recitation : R02
  */
 public class ApplicantNotFoundException extends Exception {
     /**
